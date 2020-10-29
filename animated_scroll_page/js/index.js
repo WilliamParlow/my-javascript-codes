a3827dcc06343c2f52f52b5298fd8eb82f8e62e6
Will <william.nichelatti@catolicasc.org.br>
2020-10-29 17:31:08 -0300

    Added animated scroll page

---
+++ animated_scroll_page/js/index.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+
+  let currentScroll = 0;
+  let targetScroll = 0;
+  document.querySelector('#see-more-arrow').onclick = event => {
+    const target = event.target;
+    const parentSection = target.parentElement;
+    const nextSection = parentSection.nextElementSibling;
+    targetScroll = nextSection.offsetTop;
+    currentScroll = window.scrollY;
+    softScroll();
+  }
+
+  const ease = 0.075;
+  let animationFrameId;
+  const softScroll = () => {
+    const diff = targetScroll - window.scrollY;
+    const delta = Math.abs(diff) < 0.1 ? 0 : diff * ease;
+    if (delta) {
+      currentScroll += delta;
+      animationFrameId = requestAnimationFrame(softScroll);
+      window.scrollTo(0, currentScroll);
+    } else {
+      cancelAnimationFrame(animationFrameId);
+      targetScroll = currentScroll;
+    }
+  }
+
+  window.onmousewheel = () => {
+    if (animationFrameId) {
+      cancelAnimationFrame(animationFrameId);
+    }
+  }
+}

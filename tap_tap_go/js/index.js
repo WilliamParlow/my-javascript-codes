96420cc50f75895707c1a304e8bfb1dee19d7529
Will <william.nichelatti@catolicasc.org.br>
2021-04-08 16:08:45 -0300

    Improved performance and created start menu

--- tap_tap_go/js/index.js
+++ tap_tap_go/js/index.js
@@ -8,7 +8,7 @@ window.onload = () => {
             obstacleSize: 100,
             fps: 1000 / 60,
             obstacles: [],
-            gameInterval: null,
+            gameIntervalId: null,
             player: {
                 pos: {
                     x: 0,
@@ -35,11 +35,76 @@ window.onload = () => {
                 game.player.pos.x = game.canvas_el.width / 4;
                 game.player.pos.y = game.canvas_el.height / 2;
 
+                game.createGameMenu();
+            },
+
+            createGameMenu: () => {
+                game.clear();
+
+                game.ctx.shadowColor = "transparent";
+
+                if (game.conf.isMobile) {
+                    game.ctx.font = 'bold 40px Chalkduster fantasy';
+                    game.ctx.lineWidth = 3;
+                    game.ctx.shadowBlur = 7;
+                } else {
+                    game.ctx.font = 'bold 80px Chalkduster fantasy';
+                    game.ctx.lineWidth = 5;
+                    game.ctx.shadowBlur = 7;
+                }
+
+                let textDimensions = {
+                    game: game.getTextDimensions('Start Game', {
+                        font: game.ctx.font
+                    })
+                };
+
+                textDimensions = {
+                    ...textDimensions,
+                    textHeight: textDimensions.game.height,
+                    textWidth: textDimensions.game.width,
+                    positionX: game.canvas_el.width * 0.5 - (textDimensions.game.width * 0.5),
+                    positionY: game.canvas_el.height * 0.5
+                }
+
+                game.ctx.strokeText('Start Game', textDimensions.positionX, textDimensions.positionY);
+
+                window.onkeydown = e => {
+                    game.initMenuClickEvent(e, textDimensions);
+                };
+                game.canvas_el.onclick = e => {
+                    game.initMenuClickEvent(e, textDimensions);
+                };
+                game.canvas_el.ontouchstart = e => {
+                    game.initMenuClickEvent(e, textDimensions);
+                };
+            },
+
+            initMenuClickEvent: (e, textDimensions) => {
+                if (e.type === 'click' || e.type === 'touchstart') {
+                    if (e.clientX > textDimensions.positionX && e.clientX < (textDimensions.positionX + textDimensions.textWidth) &&
+                        e.clientY < textDimensions.positionY && e.clientY > (textDimensions.positionY - textDimensions.textHeight)) {
+                        game.triggerStartGame();
+                    }
+                } else {
+                    game.triggerStartGame();
+                }
+            },
+
+            triggerStartGame: () => {
+                game.clear();
+                game.player.isAlive = true;
+                game.player.velocity = -game.conf.pushForce;
+                game.player.points = 0;
+                game.obstacles = [];
+
                 window.onkeydown = game.updatePlayerVelocity;
                 window.ontouchstart = game.updatePlayerVelocityByClickOrTouch;
                 window.onclick = game.updatePlayerVelocityByClickOrTouch;
 
-                game.gameInterval = setInterval(game.play, game.fps);
+                game.canvas_el.onclick = null;
+
+                game.gameIntervalId = requestAnimationFrame(game.play);
             },
 
             play: () => {
@@ -51,9 +116,11 @@ window.onload = () => {
                     game.drawObstacles();
                     game.drawPlayer();
                     game.drawPoints();
+                    requestAnimationFrame(game.play);
                 } else {
                     game.drawGameOverText();
-                    clearInterval(game.gameInterval);
+                    cancelAnimationFrame(game.gameIntervalId);
+                    setTimeout(game.init, 2000);
                 }
             },
 
@@ -240,6 +307,8 @@ window.onload = () => {
 // https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
 window.mobileCheck = function () {
     let check = false;
-    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    (function (a) {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
     return check;
 };
